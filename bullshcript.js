17b8159d6854bc20b1d0fef992d567f7240be426
DraculusX <DraculusX9@gmail.com>
2025-07-25 00:25:12 -0600

    2

--- bullshcript.js
+++ bullshcript.js
@@ -36,7 +36,7 @@ async function somerandomStartActions() {
 		// createButton // NAME // Button Position // posterImage // localRotation // Scale // Width // Height
   		createButton(
 				'Test01', // Name of the Button
-				new BS.Vector3(-5.46,2,-4.15), // Position of the Button
+				new BS.Vector3(-5.46,2,-4.11), // Position of the Button
 				'https://draculusx.github.io/Images/WGD.png', // Button Image
 				'https://draculusx.github.io/index.html', // Link to the Poster
 				new BS.Vector3(0,-90,0), // Local Rotation
