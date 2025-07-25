6254831d4822d6e79cd4d6f7a5d052e3dee445ac
DraculusX <DraculusX9@gmail.com>
2025-07-25 00:12:24 -0600

    messaeg

--- bullshcript.js
+++ bullshcript.js
@@ -36,12 +36,12 @@ async function somerandomStartActions() {
 		// createButton // NAME // Button Position // posterImage // localRotation // Scale // Width // Height
   		createButton(
 				'Test01', // Name of the Button
-				new BS.Vector3(-4,2,2), // Position of the Button
+				new BS.Vector3(-5.45,2,-4), // Position of the Button
 				'https://draculusx.github.io/Images/WGD.png', // Button Image
 				'https://draculusx.github.io/index.html', // Link to the Poster
 				new BS.Vector3(0,0,0), // Local Rotation
 				new BS.Vector3(.6,.6,.6), // Local Scale
-				2, // Width
+				1.4, // Width
 				1 // Height
 			);
 	}, 3000);
