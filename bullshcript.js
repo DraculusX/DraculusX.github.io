1b9463dd0b269e60f50bcdc87778b75b8b802480
FireRat <27763388+FireRat666@users.noreply.github.com>
2025-07-25 15:41:37 +1000

    update bullshcript.js

--- bullshcript.js
+++ bullshcript.js
@@ -27,19 +27,18 @@ async function somerandomStartActions() {
 				'Test01', // Name of the Button
 				new BS.Vector3(2.75,1.65,2), // Position of the Button
 				'https://draculusx.github.io/Images/RaeRaeBday.jpg', // Button Image
-				'https://firer.at/', // Link to the Poster
+				'https://draculusx.github.io/index.html', // Link to the Poster
 				new BS.Vector3(0,0,0), // Local Rotation
 				new BS.Vector3(.6,.6,.6), // Local Scale
 				2, // Width
 				1 // Height
 			);
-			// NAME // Button Position // posterImage // localRotation // Scale // Width // Height
-  		// createButton('Test02', new BS.Vector3(-1,2,-1), 'https://openclipart.org/image/800px/17880', 'https://firer.at/', new BS.Vector3(0,-90,0), new BS.Vector3(0.3, 0.3, 1), 2, 1 );
+		// createButton // NAME // Button Position // posterImage // localRotation // Scale // Width // Height
   		createButton(
 				'Test02', // Name of the Button
 				new BS.Vector3(1.75,1.65,2), // Position of the Button
 				'https://draculusx.github.io/Images/WGD.png', // Button Image
-				'https://firer.at/', // Link to the Poster
+				'https://draculusx.github.io/index.html', // Link to the Poster
 				new BS.Vector3(0,0,0), // Local Rotation
 				new BS.Vector3(.6,.6,.6), // Local Scale
 				2, // Width
